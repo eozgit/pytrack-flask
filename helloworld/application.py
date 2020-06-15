a4c44b6ef5951ec883723a17f8b3ab295b82267c
Enis Ozgit <eozgit@gmail.com>
2020-06-15 16:09:14 +0100

    Add time method
    Update gitignore for pycharm

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,27 @@
 #!flask/bin/python
+import datetime
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
 
+
+@application.route('/api/time', methods=['GET'])
+def time():
+    response = Response(json.dumps({'Output': str(datetime.datetime.now())}), mimetype='application/json', status=200)
+    return response
+
+
 @application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
+
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
 
 if __name__ == '__main__':
     flaskrun(application)
--- tests/test_application.py
+++ tests/test_application.py
@@ -2,9 +2,11 @@ import json
 import pytest
 from helloworld.application import application
 
+
 @pytest.fixture
 def client():
     return application.test_client()
+
 
 def test_response(client):
     result = client.get()
